17c88f4098907db2fe13a6cd8d64455ede6b0f9b
IsmailNWA <ismail.dinar.nwa@gmail.com>
2021-09-10 15:18:03 +0100

    1 file modified

--- src/app/style.directive.ts
+++ src/app/style.directive.ts
@@ -17,14 +17,12 @@ export class StyleDirective implements OnInit {
 
   public ngOnInit() {
     // ng-container is rendered as a comment in the DOM
-    if (this.elementRef.nativeElement.nodeName === '#comment') {
-      this.renderer.setStyle(
-        //nextSibling is the elment that was injected through the ng-container
-        // (the element that comes just after the comment)
-        this.elementRef.nativeElement.nextSibling,
-        'height',
-        '100px'
-      );
-    }
+    //nextSibling is the elment that was injected through the ng-container
+    // (the element that comes just after the comment)
+    const elementToStyle =
+      this.elementRef.nativeElement.nodeName === '#comment'
+        ? this.elementRef.nativeElement.nextSibling
+        : this.elementRef.nativeElement;
+    this.renderer.setStyle(elementToStyle, 'height', '100px');
   }
 }
