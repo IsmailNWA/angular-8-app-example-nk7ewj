1a099206b12370513c9863d273092c885074ef11
IsmailNWA <ismail.dinar.nwa@gmail.com>
2021-11-19 13:53:07 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CdkStepperModule } from '@angular/cdk/stepper/typings/stepper-module';
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, CdkStepperModule],
   bootstrap: [AppComponent],
   declarations: [AppComponent],
 })
